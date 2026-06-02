3dd8829fa1af6becb43d734ecc19e4afea310149
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2026-06-02 11:06:14 +0200

    Hetzner pipeline

--- docker/load-extensions.sh
+++ docker/load-extensions.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
-CREATE EXTENSION IF NOT EXISTS btree_gist;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<'EOF'
+DO $$
+BEGIN
+	IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'btree_gist') THEN
+		CREATE EXTENSION IF NOT EXISTS btree_gist;
+	END IF;
+END
+$$;
 EOF
